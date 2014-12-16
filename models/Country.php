96653fdeaee607f3eaf58f82164d11ec13ffc90c
YoungHuang <sean0722@msn.com>
2014-12-16 22:55:53 +0000

    gii test

--- models/Country.php
+++ models/Country.php
@@ -1,7 +1,48 @@
 <?php
+
+namespace app\models;
+
+use Yii;
+
 /**
- * Created by PhpStorm.
- * User: younghuang
- * Date: 14-12-15
- * Time: 下午9:43
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property integer $population
  */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
+}
